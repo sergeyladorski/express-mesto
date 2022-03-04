4af6e260a04487583e5d09e9ba9301dbc59b5dca
Sergey Ladorski <sergeyladorski@gmail.com>
2022-03-04 14:03:21 +0300

    cors attempt *

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -26,7 +26,8 @@ module.exports.corsHandler = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    // res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   next();
